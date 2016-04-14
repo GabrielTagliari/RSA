5ca39b8fb6a22edc9ac504894776355657a1089d
Gabriel Tagliari <gabriel.tagliari93@gmail.com>
2016-04-13 21:29:26 -0300

    Trocando para BigInteger

--- src/RSA.java
+++ src/RSA.java
@@ -1,21 +1,50 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.math.*;
 
 public class RSA {
+	
+	/*Co-primos ou Números primos entre si
+	Dois números são primos entre si quando o Máximo Divisor Comum (MDC) entre eles é 1.*/
+	
 	public static void main(String[] args) {
 		
-		Map<Integer, String> hashNumLetra = new HashMap<Integer, String>();
+		Map<BigInteger, String> NumLetra = geraTabela();
 		
-		String[] caracteres = {"A","B","C","D","E","F","G","H","I","J","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",};
-		int i = 1;
+		BigInteger primeiroPrimo = new BigInteger("17");
+		BigInteger segundoPrimo = new BigInteger("41");
+		BigInteger tamanhoConjunto = primeiroPrimo * segundoPrimo;
+		
+		BigInteger resultadoTotiente = totiente(tamanhoConjunto, primeiroPrimo, segundoPrimo);
+		
+		System.out.println(NumLetra.get(19));
+		System.out.println(tamanhoConjunto);
+		System.out.println(resultadoTotiente);
+		System.out.println(resultadoTotiente.gcd(13));
+	}
+	
+		
+	//calcula totiente
+	public static BigInteger totiente(BigInteger tamanhoConjunto, BigInteger primeiroPrimo, BigInteger segundo){
+		return (primeiroPrimo - 1) * (segundo - 1);
+	}
+	
+	//Função para gerar a tabela de relação Número-Caracter
+	public static Map<BigInteger, String> geraTabela() {
+		Map<BigInteger, String> hashNumLetra = new HashMap<BigInteger, String>();
+		
+		String[] caracteres = { "A", "B", "C", "D", "E", "F", "G", "H", "I",
+				"J", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+				"W", "X", "Y", "Z", };
+
+		BigInteger i = 1;
+
 		for (String caracter : caracteres) {
 			hashNumLetra.put(i, caracter);
 			i++;
 		}
-		
-		System.out.println(hashNumLetra.get(19));
-		
+
+		return hashNumLetra;
 	}
 }
