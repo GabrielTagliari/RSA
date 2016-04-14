56fd10a53a44ea30d9c1984d0523c1f9396fed09
Gabriel Tagliari <gabriel.tagliari93@gmail.com>
2016-04-13 21:54:05 -0300

    Corrigido BigInteger

--- src/RSA.java
+++ src/RSA.java
@@ -10,35 +10,36 @@ public class RSA {
 	
 	public static void main(String[] args) {
 		
-		Map<BigInteger, String> NumLetra = geraTabela();
+		Map<Integer, String> NumLetra = geraTabela();
 		
 		BigInteger primeiroPrimo = new BigInteger("17");
 		BigInteger segundoPrimo = new BigInteger("41");
-		BigInteger tamanhoConjunto = primeiroPrimo * segundoPrimo;
+		BigInteger tamanhoConjunto = primeiroPrimo.multiply(segundoPrimo);
 		
 		BigInteger resultadoTotiente = totiente(tamanhoConjunto, primeiroPrimo, segundoPrimo);
 		
 		System.out.println(NumLetra.get(19));
 		System.out.println(tamanhoConjunto);
 		System.out.println(resultadoTotiente);
-		System.out.println(resultadoTotiente.gcd(13));
+		System.out.println(resultadoTotiente.gcd(new BigInteger("13")));
 	}
 	
 		
 	//calcula totiente
-	public static BigInteger totiente(BigInteger tamanhoConjunto, BigInteger primeiroPrimo, BigInteger segundo){
-		return (primeiroPrimo - 1) * (segundo - 1);
+	public static BigInteger totiente(BigInteger tamanhoConjunto, BigInteger primeiro, BigInteger segundo){
+		BigInteger num1 = new BigInteger("1");
+		return (primeiro.subtract(num1)).multiply((segundo.subtract(num1)));
 	}
 	
 	//Função para gerar a tabela de relação Número-Caracter
-	public static Map<BigInteger, String> geraTabela() {
-		Map<BigInteger, String> hashNumLetra = new HashMap<BigInteger, String>();
+	public static Map<Integer, String> geraTabela() {
+		Map<Integer, String> hashNumLetra = new HashMap<Integer, String>();
 		
 		String[] caracteres = { "A", "B", "C", "D", "E", "F", "G", "H", "I",
 				"J", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
 				"W", "X", "Y", "Z", };
 
-		BigInteger i = 1;
+		int i = 1;
 
 		for (String caracter : caracteres) {
 			hashNumLetra.put(i, caracter);
